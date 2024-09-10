9bdf0a1dcf29aeefdb9b5f2d5a1f142bf2ec4b1b
Maftuna <121203266+MaftunaZokirova@users.noreply.github.com>
2024-09-09 20:07:52 -0500

    Add files via upload

---
+++ CLargs.cpp
@@ -0,0 +1,13 @@
+// Maftuna Zokirova
+
+#include <iostream> 
+
+int main(int argc, char* argv[]){
+
+    for(int i = 0; i < argc; i++){
+        std::cout << "Argument["  << i   << "]: " << argv[i] << std::endl;       
+    }
+
+
+    return 0;
+}
